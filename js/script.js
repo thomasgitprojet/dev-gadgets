3126dda70a1173e46f62983a6c4799ed5eb1b251
caravane <thomasdesse.pro@gmail.com>
2024-04-26 14:00:46 +0200

    display nbr of articles in cart

--- js/script.js
+++ js/script.js
@@ -1,13 +1,10 @@
 const btnPrev = document.querySelector(".js-btn-prev");
-console.log(btnPrev);
 
 const btnNext = document.querySelector(".js-btn-next");
-console.log(btnNext);
 
 const showImg = document.querySelector(".js-show-img");
 
 document.querySelectorAll(".js-img");
-console.log(document.querySelectorAll(".js-img"));
 
 btnPrev.addEventListener("click", function () {
 
@@ -73,19 +70,40 @@ btnNext.addEventListener("click", function () {
     }
 })
 
+/**
+ * remove the attribut src for the image display
+ * @param {string} src attribute of image
+ */
 function selectedImgAtOver (src) {  
-    console.log(src.replace("-s.png", "-l.png"));
     showImg.src = src.replace("-s.png", "-l.png")
 }
 
 const smallsImgs = document.querySelectorAll(".js-img");
-console.log(smallsImgs);
 
 for (const img of smallsImgs) {
-    console.log(img);
     img.addEventListener("mouseover", function (event) {
         let srcImg = img.src
         selectedImgAtOver(srcImg)
     })
 }
+
+const input = document.querySelector(".js-input");
+let valueInput = document.querySelector(".js-input").value;
+let valueCart = document.querySelector(".js-cart-nb");
+
+const btnAddArticle = document.querySelector(".js-btn-add");
+
+input.addEventListener("change", function (event) {
+    valueInput = event.target.value
+})
+
+btnAddArticle.addEventListener("click", function(event) {
+    valueCart.textContent = valueInput 
+    console.log(valueCart.textContent);
+    btnAddArticle.textContent = "Déjà au panier"
+    btnAddArticle.setAttribute("disabled", "true");
+    if (valueCart.textContent > 99) {
+        valueCart.textContent = "+99";
+    }
+})
 
